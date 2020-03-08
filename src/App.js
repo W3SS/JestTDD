f838c4cf41cd33019f78a0702075bee3a68ad9e7
W3SS <gswendelw3ss@gmail.com>
2020-03-08 00:59:16 -0300

    Added Feature Add Two Numbers

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
 export const hello = () => 'Hello';
+
+export const addTwoNumbers = (arg1, arg2) => {
+  if (typeof arg1 !== 'number' || typeof arg2 !== 'number') {
+    return null
+  }
+  return arg1 + arg2;
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,26 @@
-import { hello } from './App';
+import { hello, addTwoNumbers } from './App';
 
 describe('hello', () => {
   it('should be an output of hello function', () => {
     expect(hello()).toBe('Hello');
+  });
+});
+
+describe('addTwoNumbers', () => {
+  it('should be an output of addTwoNumbers function', () => {
+    expect(addTwoNumbers(1,2)).toBe(3);
+    expect(addTwoNumbers(3,4)).toBe(7);
+    expect(addTwoNumbers(5,9)).toBe(14);
+    expect(addTwoNumbers(-5,9)).toBe(4);
+    expect(addTwoNumbers(-2,-2)).toBe(-4);
+  });
+  it('should not add strings', () => {
+    expect(addTwoNumbers(2,'2')).toBe(null);
+  });
+  it('should not add objects', () => {
+    expect(addTwoNumbers(2,{})).toBe(null);
+  });
+  it('should not add arrays', () => {
+    expect(addTwoNumbers(2,[])).toBe(null);
   });
 });
